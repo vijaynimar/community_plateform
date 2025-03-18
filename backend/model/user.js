dfc63c4fa96c3ff0baf2e68604a4f11a3fd7d67d
vijaynimar <vijaynimar8@gmail.com>
2025-03-19 00:23:07 +0530

    helthy route checking

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,1 +1,8 @@
 import { Schema,model } from "mongoose";
+const userSchema=new Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true}
+})
+const user=model("user",userSchema)
+export {user}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import connection from './db.js';
+import { router } from './router/router.js';
 const app=express()
 app.use(cors())
 app.use(express.json());
 const port = 3110
+app.use(router)
 connection()
 app.listen(port,()=>{
     console.log(`Server is running on port ${port} 🚀`)
